920d92535fda824f43743de36fc46d69a7a3a89d
UngHeung <t01047377477@gmail.com>
2023-04-20 02:55:07 +0900

    feat : 로컬 세이브, 로드

--- JS/memo.js
+++ JS/memo.js
@@ -10,65 +10,29 @@ const resetBtn = document.getElementById("reset_button");
 const maxLength = 200;
 const initContent = `0/${maxLength}`;
 
-function getTitle() {
-    return title.value;
+/* reset */
+const reset = {
+    // 제목 초기화
+    title: () => {
+        title.value = "";
+    },
+
+    // 내용 초기화
+    content: () => {
+        content.value = "";
+    },
+
+    // 내용 길이 초기화
+    contentLength: () => {
+        contentLength.textContent = initContent;
+    },
+};
+
+function resetAll() {
+    reset.title();
+    reset.content();
+    reset.contentLength();
 }
-
-function setContent(value) {
-    content.value = value;
-}
-
-function getContent() {
-    return content.value;
-}
-
-function regiBtnEvent() {
-    regiBtn.addEventListener("click", () => {
-        if (check.title() && check.content()) {
-            const item = document.createElement("li");
-            const memoTitle = document.createElement("h3");
-            const memoDeleteBtn = document.createElement("button");
-            const memoContent = document.createElement("pre");
-
-            memoTitle.textContent = getTitle();
-            memoContent.textContent = getContent();
-            memoDeleteBtn.classList.add("close-button");
-
-            resetAll();
-
-            item.appendChild(memoTitle);
-            item.appendChild(memoDeleteBtn);
-            item.appendChild(memoContent);
-            memoList.appendChild(item);
-            check.emptyList();
-
-            memoDeleteBtn.addEventListener("click", () => {
-                item.remove();
-                check.emptyList();
-            });
-        }
-    });
-}
-regiBtnEvent();
-
-function resetBtnEvent() {
-    resetBtn.addEventListener("click", () => {
-        resetAll();
-    });
-}
-resetBtnEvent();
-
-function contentLengthEvent() {
-    content.addEventListener("keyup", () => {
-        let length = content.value.length;
-        if (!check.contentLength(length)) {
-            setContent(getContent().slice(0, 200));
-            length -= 1;
-        }
-        contentLength.innerText = `${length}/${maxLength}`;
-    });
-}
-contentLengthEvent();
 
 /* check */
 const check = {
@@ -109,26 +73,100 @@ const check = {
     },
 };
 
-/* reset */
-const reset = {
-    // 제목 초기화
-    title: () => {
-        title.value = "";
-    },
+let allMemo = JSON.parse(localStorage.getItem("allMemo"));
+allMemo = allMemo ?? [];
+loadMemo();
 
-    // 내용 초기화
-    content: () => {
-        content.value = "";
-    },
+function getTitle() {
+    return title.value;
+}
 
-    // 내용 길이 초기화
-    contentLength: () => {
-        contentLength.textContent = initContent;
-    },
-};
+function setContent(value) {
+    content.value = value;
+}
 
-function resetAll() {
-    reset.title();
-    reset.content();
-    reset.contentLength();
+function getContent() {
+    return content.value;
+}
+
+/* button event */
+function regiBtnEvent() {
+    regiBtn.addEventListener("click", () => {
+        if (check.title() && check.content()) {
+            addItem("add");
+        }
+    });
+}
+regiBtnEvent();
+
+function resetBtnEvent() {
+    resetBtn.addEventListener("click", () => {
+        resetAll();
+    });
+}
+resetBtnEvent();
+
+function contentLengthEvent() {
+    content.addEventListener("keyup", () => {
+        let length = content.value.length;
+        if (!check.contentLength(length)) {
+            setContent(getContent().slice(0, 200));
+            length -= 1;
+        }
+        contentLength.innerText = `${length}/${maxLength}`;
+    });
+}
+contentLengthEvent();
+
+/* load memo */
+function loadMemo() {
+    for (let i = 0; i < allMemo.length; i++) {
+        addItem("load", allMemo[i], allMemo[i].code);
+    }
+}
+
+/* add memo */
+function addItem(type, memo, code) {
+    const item = document.createElement("li");
+    const memoTitle = document.createElement("h3");
+    const memoDeleteBtn = document.createElement("button");
+    const memoContent = document.createElement("pre");
+    let memoCode;
+
+    if (type === "load") {
+        memoCode = code;
+        memoTitle.textContent = memo.titleValue;
+        memoContent.textContent = memo.titleContent;
+    } else if (type === "add") {
+        const titleValue = title.value;
+        const titleContent = content.value;
+
+        memoCode = memoList.childElementCount;
+        allMemo.push({ titleValue, titleContent, code: memoCode });
+        localStorage.setItem("allMemo", JSON.stringify(allMemo));
+        memoTitle.textContent = getTitle();
+        memoContent.textContent = getContent();
+    }
+    console.log(memoCode);
+    memoDeleteBtn.classList.add("close-button");
+
+    resetAll();
+
+    item.appendChild(memoTitle);
+    item.appendChild(memoDeleteBtn);
+    item.appendChild(memoContent);
+    memoList.appendChild(item);
+    check.emptyList();
+
+    memoDeleteBtn.addEventListener("click", () => {
+        allMemo.splice(
+            allMemo.findIndex((item) => item.code === memoCode),
+            1
+        );
+        localStorage.setItem("allMemo", JSON.stringify(allMemo));
+        item.remove();
+        check.emptyList();
+        console.log(localStorage);
+    });
+    console.log(localStorage);
 }
