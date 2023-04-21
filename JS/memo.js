53ff8623e5e8eb0ddf37ae3f5921ca34326b55de
UngHeung <t01047377477@gmail.com>
2023-04-21 15:46:24 +0900

    style : 닫기버튼 스타일 변경, 포커스스타일에 마우스오버 추가

--- JS/memo.js
+++ JS/memo.js
@@ -203,8 +203,8 @@ function addItem(type, memo, code) {
     memoDeleteBtn.classList.add("close-button");
     memoDeleteBtn.innerHTML = `
     <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <line x1="5" y1="5" x2="20" y2="20" stroke="white" stroke-width="2" />
-        <line x1="5" y1="20" x2="20" y2="5" stroke="white" stroke-width="2" />
+        <line x1="5" y1="5" x2="20" y2="20" stroke="#333333" stroke-width="3" />
+        <line x1="5" y1="20" x2="20" y2="5" stroke="#333333" stroke-width="3" />
     </svg>
     `;
 
